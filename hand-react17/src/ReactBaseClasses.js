1e77d9197512102bb467168f89158703ec910d73
zhangxing <zhangxing@kuaishou.com>
2022-07-04 11:55:48 +0800

    docs: 注释

--- hand-react17/src/ReactBaseClasses.js
+++ hand-react17/src/ReactBaseClasses.js
@@ -1,7 +1,7 @@
-import { SyncLane } from "./ReactFiberLane";
-import { scheduleUpdateOnFiber } from "./ReactFiberWorkLoop";
+import { SyncLane } from './ReactFiberLane';
+import { scheduleUpdateOnFiber } from './ReactFiberWorkLoop';
 let classComponentUpdater = {
-  //把新状态入队，第1参数是组件实例，第2个参数是新状态
+  // 把新状态入队，第1参数是组件实例，第2个参数是新状态
   enqueueSetState(inst, payload) {
     let fiber = get(inst);
 
@@ -9,30 +9,33 @@ let classComponentUpdater = {
     let eventTime = requestEventTime();
 
     // lane: 赛道，react 中有31个赛道，16种优先级
-    var lane = requestUpdateLane(fiber); //计算本次更新优先级 1
+    var lane = requestUpdateLane(fiber); // 计算本次更新优先级 1
 
-    let update = createUpdate(eventTime, lane); //创建新的更新对象
+    let update = createUpdate(eventTime, lane); // 创建新的更新对象
 
     update.payload = payload; // {number:1}
+
     enqueueUpdate(fiber, update);
+
+    // 开始调度
     scheduleUpdateOnFiber(fiber);
-  },
+  }
 };
 function enqueueUpdate(fiber, update) {
-  fiber.updateQueue.push(update); //源码里是链表结构
+  fiber.updateQueue.push(update); // 源码里是链表结构
 }
 function createUpdate(eventTime, lane) {
   return { eventTime, lane };
 }
 function requestUpdateLane(fiber) {
-  //这个地方应该按当前的事件的优先级来计算分配哪个宽道
+  // 这个地方应该按当前的事件的优先级来计算分配哪个宽道
   return SyncLane;
 }
 
 // 任务是有优先级的，优先级高的会打断优先级低的。
 // 低任务超时的时候会直接执行
 function requestEventTime() {
-  return performance.now(); //程序从启动到现在的时间，是用来计算任务的过期时间
+  return performance.now(); // 程序从启动到现在的时间，是用来计算任务的过期时间
 }
 function get(inst) {
   return inst._reactInternals;
@@ -42,7 +45,7 @@ export class Component {
     this.updater = classComponentUpdater;
   }
   setState(partialState) {
-    //调用此组件更新器的enqueueSetState入队新状态方法，参数：组件的实例和新状态
+    // 调用此组件更新器的enqueueSetState入队新状态方法，参数：组件的实例和新状态
     this.updater.enqueueSetState(this, partialState);
   }
 }
--- hand-react17/src/ReactFiberWorkLoop.js
+++ hand-react17/src/ReactFiberWorkLoop.js
@@ -6,56 +6,69 @@ var NoLanePriority = 0;
 let syncQueue = [];
 let NoContext = 0;
 let BatchedContext = 1;
-let executionContext = NoContext; //执行环境，默认值是NoContext0，非批量
+let executionContext = NoContext; // 执行环境，默认值是NoContext: 0, 表示非批量
+
 export function scheduleUpdateOnFiber(fiber) {
   let root = markUpdateLaneFromFiberToRoot(fiber);
-  //开始创建一个任务，从根节点开始进行更新
+  // 开始创建一个任务，从根节点开始进行更新, React 的更新总是从根 Fiber 开始
   ensureRootIsScheduled(root);
-  //如果当前的执行上下文环境是NoContext(非批量)并且mode不是并发的话
+
+  // 如果 只用 mode 这个一个控制变量， 那么 setTimeout里面就和外部一样, 所以还需要另一个控制变量保留 setTimeout 和外面的区别。
+  // 如果当前的执行上下文环境是NoContext(非批量)并且mode不是并发的话直接执行更新
   if (
     executionContext === NoContext &&
-    (fiber.mode & ConcurrentMode) === NoMode
+    (fiber.mode & ConcurrentMode) === NoMode // & 按位与： 1 | 1 = 1， 1 | 0 = 0，  0 | 1 = 0， 0 | 0 = 0
   ) {
     flushSyncCallbackQueue();
   }
 }
-//ReactDOM.unstable_batchedUpdate
+
+// ReactDOM.unstable_batchedUpdate
 export function batchedUpdates(fn) {
   let prevExecutionContext = executionContext;
-  executionContext |= BatchedContext;
-  fn(); //handleClick
-  executionContext = prevExecutionContext; //已经重置非批量，也就是同步了
-  //setTimeout是在这里执行的
+  executionContext |= BatchedContext; // 或等于, | 按位或； 1 | 1 = 1， 1 | 0 = 1，  0 | 1 = 1， 0 | 0 = 0
+  fn(); // handleClick
+  executionContext = prevExecutionContext; // 重置
+
+  // setTimeout是在这里执行的
+  // ...
 }
+
 function ensureRootIsScheduled(root) {
-  let nextLanes = SyncLane; //1
-  let newCallbackPriority = SyncLanePriority; //按理说应该等于最高级别赛道的优先级 12
-  var existingCallbackPriority = root.callbackPriority; //当前根节点上正在执行的更新任务的优先级
+  let nextLanes = SyncLane; // 1
+  let newCallbackPriority = SyncLanePriority; // 按理说应该等于最高级别赛道的优先级 12
+  var existingCallbackPriority = root.callbackPriority; // 当前根节点上正在执行的更新任务的优先级
   if (existingCallbackPriority === newCallbackPriority) {
-    //也是在并发模式，即使在setTimeout里也是批量的原因
-    return; //如果这个新的更新和当前根节点的已经调度的更新相等，那就直接返回，复用上次的更新，不再创建新的更新任务
+    // 这里就是并发模式下在setTimeout里也是批量的原因
+    return; // 如果这个新的更新和当前根节点的已经调度的更新相等，那就直接返回，复用上次的更新，不再创建新的更新任务
   }
   scheduleSyncCallback(performSyncWorkOnRoot.bind(null, root));
+
+  // queueMicrotask window.queueMicrotask, 使用微任务
   queueMicrotask(flushSyncCallbackQueue);
+
   root.callbackPriority = newCallbackPriority;
 }
+
 function flushSyncCallbackQueue() {
   syncQueue.forEach((cb) => cb());
   syncQueue.length = 0;
 }
-//其实就把performSyncWorkOnRoot函数添加一个队列里，等待执行
+
+// 其实就把performSyncWorkOnRoot函数添加一个队列里，等待执行
 function scheduleSyncCallback(callback) {
   syncQueue.push(callback);
 }
-//这个其实就是我们真正的渲染任务了，比较老的节点和新的节点，得到domdiff结果 更新DOM。都是这个方法里
+
+// 这个其实就是我们真正的渲染任务了，比较老的节点和新的节点，得到domdiff结果 更新DOM。都是这个方法里
 function performSyncWorkOnRoot(workInProgress) {
   let root = workInProgress;
   console.log('开始执行调合任务');
   while (workInProgress) {
     if (workInProgress.tag === ClassComponent) {
-      let inst = workInProgress.stateNode; //获取此fiber对应的类组件的实例
+      let inst = workInProgress.stateNode; // 获取此fiber对应的类组件的实例
       inst.state = processUpdateQueue(inst, workInProgress);
-      inst.render(); //得到新状态后，就可以调用render方法得到新的虚拟dom，进行dom diff 更新DOM
+      inst.render(); // 得到新状态后，就可以调用render方法得到新的虚拟dom，进行dom diff 更新DOM
     }
     workInProgress = workInProgress.child;
   }
@@ -64,7 +77,8 @@ function performSyncWorkOnRoot(workInProgress) {
 function commitRoot(root) {
   root.callbackPriority = NoLanePriority;
 }
-//根据老状态和我们更新队列计算新状态
+
+// 根据老状态和我们更新队列计算新状态
 function processUpdateQueue(inst, fiber) {
   return fiber.updateQueue.reduce((state, { payload }) => {
     if (typeof payload === 'function') {
@@ -73,6 +87,8 @@ function processUpdateQueue(inst, fiber) {
     return { ...state, ...payload };
   }, inst.state);
 }
+
+// 找到根节点
 function markUpdateLaneFromFiberToRoot(fiber) {
   let parent = fiber.return;
   while (parent) {
